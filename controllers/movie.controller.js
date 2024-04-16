059c25c8447f352df9931555ee1c50d79a83cef1
Carloslpez219 <56690399+Carloslpez219@users.noreply.github.com>
2024-04-16 11:21:50 -0600

    movies controller (getMovies, getMoviesCount, popularRecommendation, releaseRecommendation, genreRecommendation, userRecommendation, getMoviesDetail)

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,5 +1,20 @@
 var session = require('../connection');
 
+
+function getMoviesCount(req, res) {
+    session
+    .run('MATCH (m:Movie) RETURN count(m) AS movieCount')
+    .then(function(result) {
+        const movieCount = result.records[0].get('movieCount').toInt();
+        res.send({message: movieCount});
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.status(500).send({message: 'Error general'});
+    });
+}
+
+
 function getMovies(req,res){
     var resRecord = [];
 
@@ -22,6 +37,174 @@ function getMovies(req,res){
     });
 }
 
+function getMoviesDetail(req, res) {
+    var params = req.body;
+
+    var movies = params.movies.split(",");
+    var query = 'MATCH (m:Movie) WHERE m.id IN $movieIds RETURN m';
+
+    session
+    .run(query, { movieIds: movies })
+    .then(function(result) {
+        var moviesDetail = result.records.map(record => record.get('m').properties);
+
+        if (moviesDetail.length == 0) {
+            res.send({message: "Peliculas no encontradas."});
+        } else {
+            res.send(moviesDetail);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.status(500).send({message: 'Error general'});
+    });
+}
+
+
+function genreRecommendation(req, res) {
+    var params = req.body;
+    var genres = params.genres.split(",");
+    var genresList = genres.map(genre => parseInt(genre, 10)); // Asegurarse de que son enteros
+
+    var skip = params.skip;
+    var limit = params.limit;
+
+    var query = `
+    MATCH (m:Movie)
+    WHERE any(genre IN m.genre_ids WHERE genre IN $genresList)
+    RETURN m AS Movie
+    SKIP toInteger($skip)
+    LIMIT toInteger($limit)
+    `;
+
+    session
+    .run(query, { genresList: genresList, skip: skip, limit: limit })
+    .then(function(result) {
+        var moviesDetail = result.records.map(record => record.get('Movie').properties);
+
+        if (moviesDetail.length == 0) {
+            res.send({message: "Peliculas no encontradas."});
+        } else {
+            res.send(moviesDetail);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.status(500).send({message: 'Error general'});
+    });
+}
+
+
+
+
+function userRecommendation(req, res) {
+    var params = req.body;
+    var genres = params.genres.split(",");
+    var genresList = genres.map(genre => parseInt(genre));
+    var mail = params.mail;
+    var skip = parseInt(params.skip);
+    var limit = parseInt(params.limit);
+
+    var query = `
+    MATCH (u:User)-[:LIKES_GENRE]->(g:Genre)
+    WHERE g.id IN $genresList AND NOT u.credentials.mail = $mail
+    WITH u, collect(g) as likedGenres
+    UNWIND u.likedMovies AS movieId
+    RETURN DISTINCT movieId AS MovieID
+    SKIP toInteger($skip)
+    LIMIT toInteger($limit)
+    `;
+
+    session
+    .run(query, { genresList: genresList, mail: mail, skip: skip, limit: limit })
+    .then(function(result) {
+        var recommendedMovies = result.records.map(record => record.get('MovieID'));
+
+        if (recommendedMovies.length == 0) {
+            res.send({message: "Peliculas no encontradas."});
+        } else {
+            res.send(recommendedMovies);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.status(500).send({message: 'Error general'});
+    });
+}
+
+
+function popularRecommendation(req, res) {
+    var params = req.body;
+    var skip = parseInt(params.skip);
+    var limit = parseInt(params.limit);
+
+    var query = `
+    MATCH (m:Movie)
+    WHERE m.vote_average >= 8 AND m.vote_count >= 500
+    RETURN m AS Movie
+    ORDER BY m.popularity DESC
+    SKIP toInteger($skip)
+    LIMIT toInteger($limit)
+    `;
+
+    session
+    .run(query, { skip: skip, limit: limit })
+    .then(function(result) {
+        var popularMovies = result.records.map(record => record.get('Movie').properties);
+
+        if (popularMovies.length == 0) {
+            res.send({message: "Peliculas no encontradas."});
+        } else {
+            res.send(popularMovies);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.status(500).send({message: 'Error general'});
+    });
+}
+
+function releaseRecommendation(req, res) {
+    var params = req.body;
+    var skip = parseInt(params.skip);
+    var limit = parseInt(params.limit);
+
+    var query = `
+    MATCH (m:Movie)
+    WHERE m.vote_average >= 5 AND m.vote_count >= 100
+    AND m.release_date >= '2024-01-01' AND m.release_date <= '2024-04-15'
+    RETURN m AS Movie
+    ORDER BY m.release_date DESC
+    SKIP toInteger($skip)
+    LIMIT toInteger($limit)
+    `;
+
+    session
+    .run(query, { skip: skip, limit: limit })
+    .then(function(result) {
+        var recentMovies = result.records.map(record => record.get('Movie').properties);
+
+        if (recentMovies.length == 0) {
+            res.send({message: "Peliculas no encontradas."});
+        } else {
+            res.send(recentMovies);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.status(500).send({message: 'Error general'});
+    });
+}
+
+
+
+
 module.exports = {
-    getMovies
+    getMovies,
+    getMoviesCount,
+    popularRecommendation,
+    releaseRecommendation,
+    genreRecommendation,
+    userRecommendation,
+    getMoviesDetail
 }
--- routes/movie.route.js
+++ routes/movie.route.js
@@ -4,5 +4,11 @@ var movieController = require('../controllers/movie.controller');
 var api = express.Router();
 
 api.get('/getMovies',movieController.getMovies);
+api.post('/genreRecommendation',movieController.genreRecommendation);
+api.post('/userRecommendation',movieController.userRecommendation);
+api.post('/popularRecommendation',movieController.popularRecommendation);
+api.post('/releaseRecommendation',movieController.releaseRecommendation);
+api.get('/getMoviesCount',movieController.getMoviesCount);
+api.post('/getMoviesDetail',movieController.getMoviesDetail);
 
 module.exports = api;
